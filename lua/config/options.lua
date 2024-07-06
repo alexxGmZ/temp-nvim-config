dbe46c931f63e5c94ffd40783224031e71258520
alexxGmZ <algilbertgomez@gmail.com>
2024-07-06 11:24:07 +0800

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,69 @@
+local opt = vim.opt
+
+vim.cmd [[
+   set termguicolors
+   " set completeopt=menu,menuone,noselect
+   syntax on
+
+   " filetype
+   filetype on
+   filetype plugin on
+   filetype indent on
+
+   set autoread
+   set showmatch
+   set mouse=a
+   set showcmd
+   set nobackup
+   set writebackup
+   set noshowmode
+   set encoding=UTF-8
+   set fileencodings=utf8,cp1251,koi8-r,koi8-u
+]]
+
+-- column
+opt.foldcolumn = "0"
+opt.signcolumn = "yes"
+
+-- fold
+opt.foldenable = true
+opt.foldlevel = 99
+opt.foldmethod = "indent"
+
+-- indention
+opt.autoindent = true
+opt.expandtab = true
+opt.tabstop = 3
+opt.softtabstop = 3
+opt.shiftwidth = 3
+opt.smartindent = true
+
+-- linenumber
+opt.number = true
+opt.relativenumber = false
+
+-- cursor
+opt.cursorline = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.guicursor = "a:block100-blinkwait900-blinkon200-blinkoff500,i-ci:ver30"
+
+-- clipboard
+opt.clipboard = "unnamedplus"
+
+-- search
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- split
+opt.splitbelow = true
+opt.splitright = true
+
+-- linewrap
+opt.wrap = false
+opt.breakindent = true
+
+-- colorscheme
+opt.background = "dark"
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+   "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
+   config = function()
+      local treesitter = require('nvim-treesitter.configs')
+      treesitter.setup {
+         -- parser_install_dir = "/home/alex/.local/share/nvim/lazy/nvim-treesitter",
+         ensure_installed = {
+            "html",
+            "css",
+            "javascript",
+            "php",
+            "python",
+            "java",
+            "cpp",
+            "json",
+            "sql"
+         },
+         sync_install = false,
+         auto_install = true,
+         ignore_install = {
+            "bash",
+            "c",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "vim",
+            "vimdoc"
+         },
+         highlight = {
+            enable = true,
+            disable = {},
+            additional_vim_regex_highlighting = true,
+         },
+         incremental_selection = { enable = false },
+         indent = { enable = false }
+      }
+   end,
+}
